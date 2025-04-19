3c668728b0edaa107495f6b52ec994e8727414d1
Ghaithsy418 <ghaithsy418@gmail.com>
2025-04-19 10:57:00 +0300

    fixes a big bug

--- app/_hooks/useResize.ts
+++ app/_hooks/useResize.ts
@@ -1,13 +1,19 @@
+"use client"
 import { useEffect, useState } from "react";
 
 export function useResize(){
     const [widthSize, setWidthSize] = useState(0);
     useEffect(function(){
-        window.addEventListener("resize",function(){
-            if(window.innerWidth >= 1469) setWidthSize(0)
-            if(window.innerWidth <= 1469)  setWidthSize(1469)
-            if(window.innerWidth <= 875) setWidthSize(875)    
-        });
+        function handleResize(){
+                if(window.innerWidth >= 1469) setWidthSize(0)
+                if(window.innerWidth <= 1469)  setWidthSize(1469)
+                if(window.innerWidth <= 875) setWidthSize(875)    
+        }
+        handleResize();
+
+        window.addEventListener("resize",handleResize);
+
+        return ()=> window.removeEventListener("resize",handleResize)
     },[setWidthSize]);
 
     return {widthSize}
