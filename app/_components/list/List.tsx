08b8ad775f51adb6f4fcd58e8f7d22b362eb931a
Ghaithsy418 <ghaithsy418@gmail.com>
2025-04-19 18:25:59 +0300

    everything is working now

--- app/_components/list/List.tsx
+++ app/_components/list/List.tsx
@@ -7,7 +7,7 @@ import { getFriends } from "@/app/_lib/api";
 async function List() {
   const friends = await getFriends();
   return (
-    <div className="flex flex-1 flex-col justify-center border-r-1 border-gray-300/30 py-4">
+    <div className="flex flex-1 flex-col justify-center border-gray-300/30 py-4 sm:border-r-1">
       <ListHead />
       <Suspense fallback={<Spinner className="mx-auto mb-[80%]" />}>
         <ChatList friends={friends} />
